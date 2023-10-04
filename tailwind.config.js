dbfc7dc0021c57d7a742c6dedbff8464065445ba
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-10-04 15:54:34 +0430

    Add fonts, breakpoint for responsivnes, and store

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '350px',
+      md: '768px',
+      lg: '976px',
+    },
   },
   plugins: [],
 };
