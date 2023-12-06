e75d1bdf2e3b9cc1f7b3bb2a52e228aea305ac10
SaboorSohaib <abdulsaboorsohaib@gmail.com>
2023-12-06 22:22:24 +0430

    Modify unautheticated routes

--- src/routes/UnAuthenticatedRoutes.js
+++ src/routes/UnAuthenticatedRoutes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import FirstPage from '../pages/FirstPage';
 
 const UnAuthenticatedRoutes = () => (
   <Routes>
+    <Route path="/" element={<FirstPage />} />
     <Route path="/login" element={<Login />} />
     <Route path="/signup" element={<Signup />} />
   </Routes>
